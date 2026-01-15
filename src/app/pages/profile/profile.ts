90f31aa29df85183e87b6cfad5430e0b39a9635d
luiza.araqelyan <luiza.araqelyan@nrgaming.com>
2026-01-16 02:04:25 +0400

    create profile page

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { TelegramAuthService } from '../../core/services/telegram/telegram-auth.service';
+import { Icon } from "../../shared/components/icon/icon";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Icon],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
 export class Profile {
+  private readonly telegramAuthService = inject(TelegramAuthService);
 
+  protected readonly user = this.telegramAuthService.user;
+  protected readonly userAvatar = computed(() => this.user()?.profile_pic ?? 'assets/images/default-avatar.png');
+  protected readonly userName = computed(() => {
+    const user = this.user();
+    return user?.first_name && user?.last_name 
+      ? `${user.first_name} ${user.last_name}` 
+      : user?.first_name ?? 'User';
+  });
+  protected readonly userUsername = computed(() => this.user()?.username ? `@${this.user()!.username}` : '@user');
 }
